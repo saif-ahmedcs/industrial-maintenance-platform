f5db584c15c492deadd3f3538cd29fd37433808d
Saif Ahmed <seefahmed259@gmail.com>
2026-09-17 04:49:59 +0300

    feat(auth): add RolesGuard and Roles decorator

---
+++ backend/src/auth/guards/roles.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { RoleName } from '../../users/entities/role.entity';
+import { RequestUser } from '../interfaces/request-user.interface';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<RoleName[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<{ user: RequestUser }>();
+    const hasRole = request.user?.roles?.some((role) =>
+      requiredRoles.includes(role),
+    );
+
+    if (!hasRole) {
+      throw new ForbiddenException(
+        'You do not have permission to perform this action',
+      );
+    }
+
+    return true;
+  }
+}
